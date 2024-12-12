fc32c8bc66285ca6f61eca48e8e02f53271bc0d5
ybowang <ybowang@thoughtworks.com>
2024-12-12 15:50:06 +0800

    [Yuanbo] #N/A init project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['source.unsplash.com'], // 添加 Unsplash 域名
+    domains: ['source.unsplash.com'],
+    unoptimized: true,
   },
+  basePath: '/shenyu',
 }
 
 module.exports = nextConfig 
